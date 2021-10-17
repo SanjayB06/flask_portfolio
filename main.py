79ff8a68906b5dc3e6166d9d300c72cd29218968
SanjayB06 <sanjaymbharadwaj@gmail.com>
2021-10-17 15:17:18 -0700

    Sanjay - general improvements

--- main.py
+++ main.py
@@ -127,6 +127,5 @@ def signed():
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
-
 if __name__ == "__main__":
     app.run(debug=True)
