54f962b27357cb732f68b0dbb8d3aa84b56eaef7
Gavin Wu <wugavin0573@gmail.com>
2021-10-31 21:02:23 -0700

    Gavin - movieapi update

--- main.py
+++ main.py
@@ -172,11 +172,8 @@ def movieapi():
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
     data = json.loads(response.text)
-    print(data)
     return render_template("movieapi.html", moviequiz=data)
-# add code to see if the movie title exists, if it doesn't repull
 
 @app.route('/quiz/',methods=['GET','POST'])
 def quiz():
